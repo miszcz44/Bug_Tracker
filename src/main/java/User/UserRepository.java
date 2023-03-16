d1df385355438533d9208045573292a79b9ccac0
Eryk Bojaruniec <erykb412@wp.pl>
2023-03-16 12:44:08 +0100

    Added and configurated package User, configurated database connection

---
+++ src/main/java/User/UserRepository.java
@@ -0,0 +1,12 @@
+package User;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository {
+    Optional<User> findByUsername(String username);
+}
