6b6d0da282c78ffdb9209a28b8c2f5c5fb08ed46
Eryk Bojaruniec <erykb412@wp.pl>
2023-10-27 22:28:30 +0200

    Created tests for validation method

--- src/test/java/com/mcr/bugtracker/BugTrackerApplication/appuser/AppUserServiceTest.java
+++ src/test/java/com/mcr/bugtracker/BugTrackerApplication/appuser/AppUserServiceTest.java
@@ -1,23 +1,21 @@
 package com.mcr.bugtracker.BugTrackerApplication.appuser;
 
+import com.mcr.bugtracker.BugTrackerApplication.Exceptions.ApiRequestException;
 import com.mcr.bugtracker.BugTrackerApplication.registration.token.ConfirmationTokenService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.persistence.EntityManager;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,64 +23,61 @@ import static org.mockito.Mockito.when;
 class AppUserServiceTest {
 
     @Mock
-    private AppUserRepository repository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    private ConfirmationTokenService confirmationTokenService;
-    private AppUserService underTest;
-    private EntityManager entityManager;
-    private final static String USER_NOT_FOUND_MSG =
-            "user with email %s not found";
+    AppUserRepository appUserRepository;
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Mock
+    ConfirmationTokenService confirmationTokenService;
+    @Mock
+    UserProfileDtoMapper userProfileDtoMapper;
+    @Mock
+    AppUserDtoMapper appUserDtoMapper;
+    AppUserService appUserService;
+    AutoCloseable autoCloseable;
 
     @BeforeEach
     void setUp() {
-        underTest = new AppUserService(repository, bCryptPasswordEncoder, confirmationTokenService);
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        appUserService = new AppUserService(appUserRepository, bCryptPasswordEncoder, confirmationTokenService, userProfileDtoMapper, appUserDtoMapper);
     }
 
-    @Test
-    public void testLoadUserByUsernameWhenUserNotFound() {
-        String email = "non-existent-user@example.com";
-        when(repository.findByEmail(email)).thenReturn(Optional.empty());
-        assertThatThrownBy(() -> underTest.loadUserByUsername(email))
-                .isInstanceOf(UsernameNotFoundException.class)
-                .hasMessageContaining(String.format(USER_NOT_FOUND_MSG, email));
+    @AfterEach
+    void tearUp() throws Exception {
+        autoCloseable.close();
     }
-
     @Test
-    public void testLoadUserByUsernameWhenUserExists() {
-        String email = "existing-user@example.com";
-        AppUser user = new AppUser("1","2", email, "password", AppUserRole.ADMIN);
-        when(repository.findByEmail(email)).thenReturn(Optional.of(user));
-        UserDetails userDetails = underTest.loadUserByUsername(email);
-        assertEquals(email, userDetails.getUsername());
+    public void checkIfEmailTakenOrNotConfirmedTest_EmailNotTaken() {
+        //given
+        AppUser appUser = new AppUser();
+        appUser.setEmail("123");
+        when(appUserRepository.findByEmail(appUser.getEmail())).thenReturn(Optional.empty());
+        //when
+        appUserService.checkIfEmailTakenOrNotConfirmed(appUser);
+        //then
     }
-
     @Test
-    public void testCheckIfEmailTakenOrNotConfirmedWhenNotConfirmed() {
-        String email = "existing-user@example.com";
-        AppUser user = new AppUser("1","2", email, "password", AppUserRole.ADMIN);
-        user.setEnabled(false); // it's also false by default
-        when(repository.findByEmail(email)).thenReturn(Optional.of(user));
-        assertThatThrownBy(() -> underTest.checkIfEmailTakenOrNotConfirmed(user))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("Confirm email");
+    public void checkIfEmailTakenOrNotConfirmedTest_EmailNotConfirmed() {
+        //given
+        AppUser appUser = new AppUser();
+        appUser.setEmail("123");
+        when(appUserRepository.findByEmail(appUser.getEmail())).thenReturn(Optional.of(appUser));
+        //when
+        ApiRequestException exception =
+                assertThrows(ApiRequestException.class, () -> appUserService.checkIfEmailTakenOrNotConfirmed(appUser));
+        //then
+        assertEquals(exception.getMessage(), "Confirm email");
     }
-
     @Test
-    public void testCheckIfEmailTakenOrNotConfirmedWhenEmailTaken() {
-        String email = "existing-user@example.com";
-        AppUser user = new AppUser("1","2", email, "password", AppUserRole.ADMIN);
-        user.setEnabled(true);
-        when(repository.findByEmail(email)).thenReturn(Optional.of(user));
-        assertThatThrownBy(() -> underTest.checkIfEmailTakenOrNotConfirmed(user))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("email already taken");
-    }
-
-    @Test
-    void signUpUser() {
-    }
-
-    @Test
-    void enableAppUser() {
+    public void checkIfEmailTakenOrNotConfirmedTest_EmailTaken() {
+        //given
+        AppUser appUser = new AppUser();
+        appUser.setEmail("123");
+        appUser.setEnabled(true);
+        when(appUserRepository.findByEmail(appUser.getEmail())).thenReturn(Optional.of(appUser));
+        //when
+        ApiRequestException exception =
+                assertThrows(ApiRequestException.class, () -> appUserService.checkIfEmailTakenOrNotConfirmed(appUser));
+        //then
+        assertEquals(exception.getMessage(), "Email already taken");
     }
 }
